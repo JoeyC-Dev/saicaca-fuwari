36ab864e4a7a43c8ded52175c277ce846717e610
Joey Chen <142381267+JoeyC-Dev@users.noreply.github.com>
2026-02-02 06:49:08 +0800

    add missing line in themes configuration
    Signed-off-by: Joey Chen <142381267+JoeyC-Dev@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -89,6 +89,7 @@ export default defineConfig({
 		}),
 		expressiveCode({
 			// themes: [expressiveCodeConfig.theme, expressiveCodeConfig.theme],
+			themes: expressiveCodeConfig.themes,
 			plugins: [
 				pluginCollapsibleSections(),
 				pluginLineNumbers(),
