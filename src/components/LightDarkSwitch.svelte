0533ff79df9bf1c2e86151548a6ed66330d0ff14
Joey Chen <142381267+JoeyC-Dev@users.noreply.github.com>
2026-02-03 01:34:30 +0800

    fix: astro check issue
    Signed-off-by: Joey Chen <142381267+JoeyC-Dev@users.noreply.github.com>
    
    fix: astro check issue

--- src/components/LightDarkSwitch.svelte
+++ src/components/LightDarkSwitch.svelte
@@ -19,6 +19,8 @@ import {
 import { onMount } from "svelte";
 import type { LIGHT_DARK_MODE } from "@/types/config.ts";
 
+let {} = $props();
+
 const seq: LIGHT_DARK_MODE[] = [LIGHT_MODE, DARK_MODE, AUTO_MODE];
 let mode: LIGHT_DARK_MODE = $state(
 	typeof document !== 'undefined'
